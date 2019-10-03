12effb22de9db5428b6756695d229029a48fe6be
Brian Douglas <bdougie@users.noreply.github.com>
2019-10-03 10:11:42 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function getWeekNumber(d) {
     // Copy date so don't modify original
-    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+   d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
     // Set to nearest Thursday: current date + 4 - current day number
     // Make Sunday's day number 7
     d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay()||7));
